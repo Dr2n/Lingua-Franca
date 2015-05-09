642d3b50acd39ce6cc63c160ba81e0cb1e837461
Dr2n <darrenfu@outlook.com>
2015-05-10 09:14:47 +1000

    Bug fix

--- pullFromRepo.php
+++ pullFromRepo.php
@@ -1,6 +1,7 @@
 <?php
+	`rm -rf Lingua-Franca-Redesign-Repo`;
 	`git clone https://github.com/Dr2n/Lingua-Franca-Redesign-Repo.git`;
-	'mv Lingua-Franca-Design-Repo/* .';
-	`rmdir -rf Lingua-Franca-Design-Repo`;
+	'mv Lingua-Franca-Redesign-Repo/* .';
+	`rm -rf Lingua-Franca-Redesign-Repo`;
 	echo '<h2>Upated with GitHub Repository</h2>';
 ?>
