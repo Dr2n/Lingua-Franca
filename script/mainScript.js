1cb1e72707bc01f5a03bbdb63a026d7fde050f10
Darren Fu <darrenfu@outlook.com>
2015-05-25 00:42:12 +1000

    Events Heading
    -Improvements in responsiveness to window size
    -Removes Z translation for IE

--- script/mainScript.js
+++ script/mainScript.js
@@ -4,6 +4,20 @@ function init(){
 	console.log("Page Loaded.");
 	window.popupOpen = false;
 	document.getElementById('signUpButton').addEventListener("click", togglePopup, false);
+
+	/*Fixing Browser Compatibility Issues*/
+	if (!(window.ActiveXObject) && "ActiveXObject" in window){
+
+		var backgrounds = document.getElementsByClassName('depth2');
+    	for (var i = 0; i < backgrounds.length; i++){
+    		backgrounds[i].style.transform = 'none';
+    	}
+
+    	var midgrounds = document.getElementsByClassName('depth1');
+    	for (var i = 0; i < midgrounds.length; i++){
+    		midgrounds[i].style.transform = 'none';
+    	}
+	}
 }
 
 function togglePopup(event){
