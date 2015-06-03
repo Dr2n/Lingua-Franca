eed26f19492ff090986e28d7f5f4eac6c630c125
Darren Fu <darrenfu@outlook.com>
2015-06-03 23:59:30 +1000

    dghdgfh
    sdfg

--- script/sendEmail.php
+++ script/sendEmail.php
@@ -2,12 +2,12 @@
 
 	$to = 'darren.yx.fu@gmail.com';
 	$subject = $_POST['senderSubject'];
-	$message = wordwrap($_POST['senderMessage'], 70, '\n..');
+	$message = wordwrap($_POST['senderMessage'], 70, '\n.');
 
-	$headers = "From : inbox@linguafranca.dreamhosters.com" . '\n..';
-	$headers .= "Reply-To:" . $_POST['senderEmail'] . '\n..';
-	if ($_POST['CCCheck']){
-		$headers .= 'CC: ' . $_POST['senderEmail'] . '\n..';
+	$headers = "From : inbox@linguafranca.dreamhosters.com" . '\n.';
+	$headers .= "Reply-To:" . $_POST['senderEmail'] . '\n.';
+	if (isset($_POST['CCCheck'])){
+		$headers .= 'CC: ' . $_POST['senderEmail'] . '\n.';
 	}
 
 
